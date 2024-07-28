eaf2afca618ddd0fb0db2ab6ba8dd099cb21e297
wagnerqriar <wagner.sampaio@qriarlabs.com>
2024-07-27 23:33:17 -0300

    refector: Compounding the job vacancy card

---
+++ src/const/index.ts
@@ -0,0 +1,38 @@
+export const jobsData = [
+    {
+      id: 1,
+      title: "Professor de Matemática",
+      description: "Responsável por ministrar aulas de Matemática no Campus Oriximiná, além de desenvolver e aplicar avaliações.",
+      location: "Campus Oriximiná",
+      postedAgo: "2 dias atrás",
+    },
+    {
+      id: 2,
+      title: "Coordenador Pedagógico",
+      description: "Gerenciar e coordenar atividades pedagógicas e administrativas no Campus Santarém, desenvolvendo estratégias para melhoria da qualidade do ensino.",
+      location: "Campus Santarém",
+      postedAgo: "1 semana atrás",
+    },
+    {
+      id: 3,
+      title: "Instrutor de Educação Física",
+      description: "Promover e conduzir atividades físicas e esportivas no Campus Juruti, além de desenvolver programas de treinamento e saúde.",
+      location: "Campus Juruti",
+      postedAgo: "3 dias atrás",
+    },
+    {
+      id: 4,
+      title: "Tutor de Ensino a Distância",
+      description: "Acompanhar e orientar alunos do Campus Alenquer em cursos de Ensino a Distância, auxiliando no desenvolvimento acadêmico e na realização de atividades.",
+      location: "Campus Alenquer",
+      postedAgo: "1 mês atrás",
+    },
+    {
+      id: 5,
+      title: "Assistente Administrativo",
+      description: "Auxiliar nas atividades administrativas do Campus Óbidos, gerenciando documentos, atendendo alunos e prestando suporte às equipes acadêmicas.",
+      location: "Campus Óbidos",
+      postedAgo: "1 semana atrás",
+    },
+  ];
+  
