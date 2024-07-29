f7966d179d406d88bade208d4e8ff8957febae20
SangelaSantos <sangelaufopa@gmail.com>
2024-07-29 12:38:29 -0300

    Feat: Profile screen base

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { ButtonLabel, ImageCover, DivTop, ImageWrapper, UploadButton, ButtonCoverLabel } from './style';
+import { FaCamera } from "react-icons/fa";
 
-const Profile = () => {
+const Profile: React.FC = () => {
+  const [profileImage, setProfileImage] = useState<string | undefined>(undefined);
+  const [coverImage, setCoverImage] = useState<string | undefined>(undefined);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setProfileImage(imageUrl);
+    }
+  };
+  const CoverImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setCoverImage(imageUrl);
+    }
+  };
+
   return (
-    <h1>oi</h1>
-  )
-}
+    <DivTop>
+      <ImageCover>
+      {coverImage ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={coverImage} alt="Cover" style={{ width: '100%', height: '100px', objectFit: 'cover' }} />
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src="/cover.png" alt="Cover" style={{ width: '100%', height: '100px', objectFit: 'cover' }} />
+        )}
+        <UploadButton
+          type="file"
+          accept="image/*"
+          id="imageUpload"
+          onChange={CoverImageChange}
+        />
+        <ButtonCoverLabel htmlFor="imageUpload"><FaCamera />Adicionar foto de capa</ButtonCoverLabel>
+      </ImageCover>
+      <ImageWrapper>
+        {profileImage ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={profileImage} alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src="/profile.png" alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+        )}
+        <UploadButton
+          type="file"
+          accept="image/*"
+          id="imageUpload"
+          onChange={handleImageChange}
+        />
+        <ButtonLabel htmlFor="imageUpload"><FaCamera /></ButtonLabel>
+      </ImageWrapper>
+      
+    </DivTop>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/profile/style.ts
+++ src/components/profile/style.ts
@@ -1,5 +1,67 @@
 import styled from "styled-components";
 
-export const DivIcon = styled.div`
-    color: red;
-`
+export const DivTop = styled.div`
+    background-color: white;
+    margin: 60px auto;
+    width: 80%;
+    height: 300px;
+    border-radius: 10px;
+    position: relative;
+`;
+
+export const ImageWrapper = styled.div`
+  position: absolute;
+  top: 50px; 
+  left: 100px; 
+  transform: translateX(-50%); 
+  width: 150px; 
+  height: 150px; 
+  border-radius: 50%;
+  overflow: hidden;
+  border: 2px solid #fff; 
+  background-color: #f0f0f0; 
+`;
+
+export const UploadButton = styled.input`
+  display: none;
+`;
+
+export const ButtonLabel = styled.label`
+    position: absolute;
+  bottom: 25px;
+  right: 15px;  
+  background-color: white; 
+  color: #228b22;
+  padding: 5px;
+  border-radius: 50%;
+  cursor: pointer;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center; 
+  width: 30px; 
+  height: 30px; 
+
+`;
+
+export const ImageCover = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100px; 
+
+`;
+export const ButtonCoverLabel = styled.label`
+    position: absolute;
+  bottom: 10px; 
+  right: 10px;  
+  background-color: rgba(0, 0, 0, 0.5); 
+  color: white;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  
+`;
