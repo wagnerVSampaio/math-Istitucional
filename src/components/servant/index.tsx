edeb7cc343f2377054aaf41085e6ea3ab0369da9
SangelaSantos <sangelaufopa@gmail.com>
2024-07-31 16:01:31 -0300

    Feat: Changes to the servant registration screen

--- src/components/servant/index.tsx
+++ src/components/servant/index.tsx
@@ -1,17 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import React, { useState } from "react";
-import {
-  Checkbox,
-  DatePicker,
-  Form,
-  Input,
-  Upload,
-  Button,
-  Flex
-} from "antd/lib";
-import { PlusOutlined } from "@ant-design/icons/lib";
+import { Checkbox, DatePicker, Form, Input, Button, Flex } from "antd/lib";
 import Link from "next/link";
 import type { CheckboxProps } from "antd/lib";
-import { P2styled } from "./style";
+import { ButtonLabelDate, DateBirthUpload, UploadButtonDate, } from "./style";
+
 
 type FieldType = {
   nome?: string;
@@ -32,7 +25,19 @@ const normFile = (e: any) => {
 
 const NavServidor = () => {
   const [value, setValue] = useState(1);
+  const [checked, setChecked] = useState(true);
+  const [disabled, setDisabled] = useState(false);
+  const [RegisterImage, setRegisterImage] = useState<string | undefined>(
+    undefined
+  );
 
+  const handleRegisterImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setRegisterImage(imageUrl);
+    }
+  };
   const [componentSize, setComponentSize] = useState<SizeType | "default">(
     "default"
   );
@@ -40,9 +45,6 @@ const NavServidor = () => {
   const onFormLayoutChange = ({ size }: { size: SizeType }) => {
     setComponentSize(size);
   };
-
-  const [checked, setChecked] = useState(true);
-  const [disabled, setDisabled] = useState(false);
 
   const toggleChecked = () => {
     setChecked(!checked);
@@ -58,153 +60,150 @@ const NavServidor = () => {
   };
 
   return (
-      <Form
-        labelCol={{ span: 1 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
-        initialValues={{ size: componentSize }}
-        onValuesChange={onFormLayoutChange}
-        size={componentSize as SizeType}
-        className="max-w-[600px]"
-      >
-          <div className="flex">
-            <div className="mr-[100px]">
-              <P2styled className="mb-[3px]">
-                Nome completo <strong className="text-red-500"> *</strong>
-              </P2styled>
-              <Form.Item<FieldType>
-                rules={[
-                  { required: true, message: "Por favor, insira o nome!" },
-                ]}
-              >
-                <Input className="w-[350px]"/>
-              </Form.Item>
+    <Form
+      labelCol={{ span: 1 }}
+      wrapperCol={{ span: 14 }}
+      layout="horizontal"
+      initialValues={{ size: componentSize }}
+      onValuesChange={onFormLayoutChange}
+      size={componentSize as SizeType}
+      className="max-w-[600px]"
+    >
+      <div className="flex">
+        <div className="mr-[75px]">
+          <p className="mb-[3px]">
+            Nome completo <strong className="text-red-500"> *</strong>
+          </p>
+          <Form.Item<FieldType>
+            rules={[{ required: true, message: "Por favor, insira o nome!" }]}
+          >
+            <Input className="w-[350px]" />
+          </Form.Item>
 
-              <p className="mt-[3px] mb-[3px]">
-                E-mail <strong className="text-red-500"> *</strong>
-              </p>
-              <Form.Item<FieldType>
-                name="email"
-                rules={[
-                  { required: true, message: "Por favor, insira o e-mail!" },
-                ]}
-              >
-                <Input className="w-[350px]"/>
-              </Form.Item>
-            </div>
+          <p className="mt-[3px] mb-[3px]">
+            E-mail <strong className="text-red-500"> *</strong>
+          </p>
+          <Form.Item<FieldType>
+            name="email"
+            rules={[{ required: true, message: "Por favor, insira o e-mail!" }]}
+          >
+            <Input className="w-[350px]" />
+          </Form.Item>
+        </div>
 
-            <div>
-              <p className="mb-[3px]">Foto (opcional) </p>
-              <Form.Item valuePropName="fileList" getValueFromEvent={normFile}>
-                <Upload action="/upload.do" listType="picture-card">
-                  <button
-                    style={{ border: 0, background: "none", width: "150px" }}
-                    type="button"
-                  >
-                    <PlusOutlined />
-                    <div className="mt-[8px]">Carregar</div>
-                  </button>
-                </Upload>
-              </Form.Item>
-            </div>
-          </div>
+        <div >
+          <p className="mb-[3px]">Foto (opcional) </p>
+          <Form.Item>
+            <ButtonLabelDate htmlFor="registerImageUpload">
+              <DateBirthUpload className="relative">
+                <img
+                  src={RegisterImage || "background-upload.png"}
+                  alt="Register"
+                  className="w-full object-cover"
+                />
+                <UploadButtonDate 
+                  type="file"
+                  accept="image/*"
+                  id="registerImageUpload"
+                  onChange={handleRegisterImageChange}
+                  style={{display: "none"}}
+                />
+              </DateBirthUpload>
+            </ButtonLabelDate>
+          </Form.Item>
+        </div>
+      </div>
 
-          <div className="flex">
-            <div className="mr-[50px]">
-              <p className="mb-[3px]">
-                Telefone <strong className="text-red-500"> *</strong>
-              </p>
-              <Form.Item<FieldType>
-                name="phone"
-                rules={[
-                  { required: true, message: "Por favor, insira o telefone!" },
-                ]}
-              >
-                <Input className="w-[250px]"/>
-              </Form.Item>
-            </div>
+      <div className="flex">
+        <div className="mr-[50px]">
+          <p className="mb-[3px]">
+            Telefone <strong className="text-red-500"> *</strong>
+          </p>
+          <Form.Item<FieldType>
+            name="phone"
+            rules={[
+              { required: true, message: "Por favor, insira o telefone!" },
+            ]}
+          >
+            <Input className="w-[250px]" />
+          </Form.Item>
+        </div>
 
-            <div>
-              <p className="mb-[3px]">
-                Data de nascimento <strong className="text-red-500"> * </strong>
-              </p>
-                  <Form.Item>
-                    <DatePicker
-                      className="custom-date-picker w-[250px]"
-                      placeholder="Selecione a data de nascimento"
-                    />
-                  </Form.Item >
-            </div>
-          </div>
-          <div className="flex">
-                    <div className="mr-[50px]">
-                      <p className="mb-[3px]">
-                        Senha <strong className="text-red-500"> *</strong>
-                      </p>
-                      <Form.Item<FieldType>
-                        name="password"
-                        rules={[
-                          {
-                            required: true,
-                            message: "Por favor, insira a senha!",
-                          },
-                        ]}
-                      >
-                        <Input.Password className="w-[250px]" />
-                      </Form.Item>
-                    </div>
-                    <div>
-                      <p className="mb-[3px]">
-                        Confirmação de senha{" "}
-                        <strong className="text-red-500"> *</strong>
-                      </p>
-                      <Form.Item<FieldType>
-                        name="passwordconfirmation"
-                        rules={[
-                          {
-                            required: true,
-                            message:
-                              "Por favor, insira a confirmação da senha!",
-                          },
-                        ]}
-                      >
-                        <Input.Password className="w-[250px] bg-white" />
-                      </Form.Item>
-                    </div>
-              </div>
-          <div className="mb-[20px] flex">
-            <Checkbox
-              checked={checked}
-              disabled={disabled}
-              onChange={onChangeCheck}
-              className="mr-[20px]"
+        <div>
+          <p className="mb-[3px]">
+            Data de nascimento <strong className="text-red-500"> * </strong>
+          </p>
+          <Form.Item>
+            <DatePicker
+              className="custom-date-picker w-[250px]"
+              placeholder="Selecione a data de nascimento"
             />
-            <p>Para prosseguir, por favor, clique no botão{" "}
-              <strong className="text-customGreen">Aceitar Termos</strong>{" "}
-              abaixo e confirme sua concordância com nossos termos de serviço e
-              política de privacidade.</p>
-          </div>
-          <div className="mt-[40px]">
-            <Flex gap="small" wrap>
-              <Link href={"/"}>
-                <Button
-                  className="w-[250px] mr-[40px] text-customGreen font-bold border-[1px] border-[#006b3f]"
-                >
-                  Voltar
-                </Button>
-              </Link>
-              <Link href={"./teste"}>
-                <Button
-                  type="primary"
-                  className="w-[250px] font-extrabold"
-                >
-                  Criar conta
-                </Button>
-              </Link>
-            </Flex>
-          </div>
-      </Form>
-    
+          </Form.Item>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="mr-[50px]">
+          <p className="mb-[3px]">
+            Senha <strong className="text-red-500"> *</strong>
+          </p>
+          <Form.Item<FieldType>
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Por favor, insira a senha!",
+              },
+            ]}
+          >
+            <Input.Password className="w-[250px]" />
+          </Form.Item>
+        </div>
+        <div>
+          <p className="mb-[3px]">
+            Confirmação de senha <strong className="text-red-500"> *</strong>
+          </p>
+          <Form.Item<FieldType>
+            name="passwordconfirmation"
+            rules={[
+              {
+                required: true,
+                message: "Por favor, insira a confirmação da senha!",
+              },
+            ]}
+          >
+            <Input.Password className="w-[250px] bg-white" />
+          </Form.Item>
+        </div>
+      </div>
+      <div className="mb-[20px] flex">
+        <Checkbox
+          checked={checked}
+          disabled={disabled}
+          onChange={onChangeCheck}
+          className="mr-[20px]"
+        />
+        <p>
+          Para prosseguir, por favor, clique no botão{" "}
+          <strong className="text-customGreen">Aceitar Termos</strong> abaixo e
+          confirme sua concordância com nossos termos de serviço e política de
+          privacidade.
+        </p>
+      </div>
+      <div className="mt-[40px]">
+        <Flex gap="small" wrap>
+          <Link href={"/"}>
+            <Button className="w-[250px] mr-[40px] text-customGreen font-bold border-[1px] border-[#006b3f]">
+              Voltar
+            </Button>
+          </Link>
+          <Link href={"./teste"}>
+            <Button type="primary" className="w-[250px] font-extrabold">
+              Criar conta
+            </Button>
+          </Link>
+        </Flex>
+      </div>
+    </Form>
   );
 };
 
